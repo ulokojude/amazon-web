50b146cc73711febecee7441296fab3ea01e9ea1
ulokojude <myadonaitech@gmail.com>
2026-02-01 17:03:33 -0800

    four

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -48,7 +48,7 @@ products.forEach((product) =>{
 
       <button 
         class="add-to-cart-button button-primary js-add-to-cart-button"
-        data-product-name="${product.name}">
+        data-product-id="${product.id}">
         Add to Cart
       </button>
     </div>
@@ -57,8 +57,26 @@ products.forEach((product) =>{
 
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
-document.querySelectorAll(".js-add-to-cart-button").forEach((button) =>{
-  button.addEventListener("click", () => {
-    
+document.querySelectorAll(".js-add-to-cart-button").forEach((button) => {
+  button.addEventListener('click', () => {
+    const productId = button.dataset.productId;
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productId: productId,
+        quantity: 1
+      });
+    }
+
+    console.log(cart);
   });
 });
